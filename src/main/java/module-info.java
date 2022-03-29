d188b0ac0de335b007347722aa4ea6ffe42dc946
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2022-03-30 01:07:28 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.lecture02fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.lecture02fx to javafx.fxml;
-    exports com.example.lecture02fx;
-}
